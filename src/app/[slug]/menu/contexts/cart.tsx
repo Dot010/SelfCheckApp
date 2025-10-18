259a9c398c9febe6a766278327b4927e65cbe72a
jonathan <jonathan2500@outlook.pt>
2025-10-18 17:16:40 -0300

    feat: add initial cart item components  [wihtouth funcionallity]

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -1,53 +1,63 @@
 "use client";
 
 import { Product } from "@prisma/client";
-import  { createContext, useState } from "react";
+import { createContext, ReactNode, useState } from "react";
 
-interface CartProduct extends Pick<Product , "id" | "name" | "price" | "imageUrl"> {
-    quantity: number
-    
+export interface CartProduct
+  extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
+  quantity: number;
 }
 
-
-export interface InCartContext{
-    isOpen: boolean;
-    products: CartProduct[];
-    toggleCart: () => void;
-    addProduct: (product: CartProduct) => void
+export interface ICartContext {
+  isOpen: boolean;
+  products: CartProduct[];
+  toggleCart: () => void;
+  addProduct: (product: CartProduct) => void;
 }
 
-export const CartContext = createContext<InCartContext>({
-    isOpen: false,
-    products: [],
-    toggleCart: () => { },
-    addProduct: () => {},
+export const CartContext = createContext<ICartContext>({
+  isOpen: false,
+  products: [],
+  toggleCart: () => {},
+  addProduct: () => {},
 });
 
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-
-export const CartProvider = ({ children }: { children: React.ReactNode }) => {
-    const [products, setProducts] = useState<CartProduct[]>([]);
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-    
-    const toggleCart = () => {
-        setIsOpen(prev => !prev);
-    };
-
-    const addProduct = (product: CartProduct) => {
-    setProducts(prev => [...prev, product]);
-    
-}
-    return (
-        <CartContext.Provider
-            value={{
-                isOpen,
-                products,
-                toggleCart,
-                addProduct,
-            }}
-        >
-            {children}
-        </CartContext.Provider>
+  const toggleCart = () => {
+    setIsOpen((prev) => !prev);
+  };
+  const addProduct = (product: CartProduct) => {
+    const productIsAlreadyOnTheCart = products.some(
+      (prevProduct) => prevProduct.id === product.id,
     );
+    if (!productIsAlreadyOnTheCart) {
+      return setProducts((prev) => [...prev, product]);
+    }
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id === product.id) {
+          return {
+            ...prevProduct,
+            quantity: prevProduct.quantity + product.quantity,
+          };
+        }
+        return prevProduct;
+      });
+    });
+  };
+  return (
+    <CartContext.Provider
+      value={{
+        isOpen,
+        products,
+        toggleCart,
+        addProduct,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 };
-
